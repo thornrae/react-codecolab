17881f06a3dcbee8660e1b2fbee102d452e418b0
Taylor Thornton <thornrae@gmail.com>
2021-07-21 23:41:31 -0700

    notes on each component of what should ultimately render and what we will need

--- src/components/colab/colab.js
+++ src/components/colab/colab.js
@@ -1,14 +1,16 @@
 import React from 'react';
-import Chat from '../chat/chat.js'
+import Chat from '../chat/chat.js';
 
 //import socket.io
 //pass socket.id as props to chat 
 
 const Colab = () => {
 
+
 return (
   <>
   <h1>CoLab Room</h1>
+  <p>this page should display question passed to this component from whatever was selected from Create component (as props) </p>
   <Chat />
   </>
 )
--- src/components/lobby/lobby.js
+++ src/components/lobby/lobby.js
@@ -3,7 +3,18 @@ import React from 'react';
 const Lobby = () => {
 
 return (
-  <h1>LOBBY</h1>
+  <>
+  <h3>LOBBY</h3>
+  <div>
+    <ul>
+      <li>SAMPLE: Room || Question: Linked Lists || 2 users present</li>
+      <li>Information above needs to be managed in some kind of state</li>
+      <li>We will need a function that holds reference to all questions currently being worked on</li>
+      <li>Perhaps we determine this by adding an "active" property to our question objects that changes depending on if their are users interacting with that question</li>
+    </ul>
+
+  </div>
+  </>
 )
 
   
