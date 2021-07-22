03c3029359049fb6cf80a076a92cc654a8b28888
Taylor Thornton <thornrae@gmail.com>
2021-07-22 01:46:56 -0700

    added button

--- src/components/lobby/lobby.js
+++ src/components/lobby/lobby.js
@@ -1,5 +1,9 @@
 import React from 'react';
 
+const enter = () => {
+  alert('entering room...')
+}
+
 const Lobby = () => {
 
 return (
@@ -7,7 +11,8 @@ return (
   <h3>LOBBY</h3>
   <div>
     <ul>
-      <li>SAMPLE: Room || Question: Linked Lists || 2 users present</li>
+      <li>Room || Question: Linked Lists || 2 users present</li>
+      <button onClick={() => enter()}>Join Room</button>
       <li>Information above needs to be managed in some kind of state</li>
       <li>We will need a function that holds reference to all questions currently being worked on</li>
       <li>Perhaps we determine this by adding an "active" property to our question objects that changes depending on if their are users interacting with that question</li>
