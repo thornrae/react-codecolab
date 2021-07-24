6e0f25566e80df470466384c392dc9bb4fa72856
Terrence Jamal Simms <69491583+TJSOmega@users.noreply.github.com>
2021-07-24 12:07:39 -0700

    Added several core functionality features (User creation, Rooms creation, Rooms join)

--- src/pages/lobby/lobby.js
+++ src/pages/lobby/lobby.js
@@ -1,34 +1,68 @@
-import React from 'react';
+import {React, useEffect} from 'react';
 import Button from '@material-ui/core/Button';
 import { useHistory } from 'react-router-dom'
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
 
-
-const Lobby = () => {
+const Lobby = (props) => {
   const history = useHistory()
-  const onClick = () => {
+
+  const socket = props.data
+
+
+
+
+  const link = () => {
     history.push('/create')
   }
 
-return (
-  <>
-  <h3>LOBBY</h3>
-  <div>
+  const roomSelect = (e) => {
+    let roomName = e.target.childNodes[0].textContent.split(' || ')
+    socket.emit('join-room', roomName[1])
+    console.log(roomName)
+    console.log(e.target.childNodes[0].textContent)
+    console.log(e)
 
-  <Button onClick={onClick}>Create New Lobby</Button>
+    history.push(`/colab/${roomName[1]}`)
+
+  }
+
+  useEffect(() => {
+  
+
+  }, [props.rooms])
+
+  return (
+    <>
 
 
-    {/* <ul>
+      <h3>LOBBY</h3>
+
+      <List component="nav">
+        {props.rooms.map((room) =>
+          <ListItem button onClick={roomSelect} value={room.name} key={room.room_id}>
+            <ListItemText primary={`${room.name} || ${room.room_id} || ${room.activeUsers}`} />
+          </ListItem>)}
+      </List>
+
+      <div>
+
+        <Button onClick={link}>Create New Lobby</Button>
+
+
+        {/* <ul>
       <li>SAMPLE: Room || Question: Linked Lists || 2 users present</li>
       <li>Information above needs to be managed in some kind of state</li>
       <li>We will need a function that holds reference to all questions currently being worked on</li>
       <li>Perhaps we determine this by adding an "active" property to our question objects that changes depending on if their are users interacting with that question</li>
     </ul> */}
 
-  </div>
-  </>
-)
+      </div>
+    </>
+  )
 
-  
+
 
 }
 
